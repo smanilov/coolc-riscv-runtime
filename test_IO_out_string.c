85379c7366db47b5b4c7b2482e98e9a53ad1fa62
Stan Manilov <stanislav.manilov@gmail.com>
2025-07-04 16:01:21 +0300

    Introduce print_string for reuse in tests

--- test_IO_out_string.c
+++ test_IO_out_string.c
@@ -10,7 +10,7 @@ typedef struct {
     int object_size;
     void* dispatch_table;
     Int* length;
-    char content[32]; // for testing purposes, length is fixed at 32
+    char content[256]; // for testing purposes, length is fixed at 256
 } String;
 
 extern void IO_out_string(String* x);
@@ -21,20 +21,37 @@ void assign_string_content(String* string, char* content, int length) {
     }
 }
 
-int main() {
-    Int length;
-    length.class_tag = 2;
-    length.object_size = 4;
-    length.dispatch_table = 0;
-    length.value = 6;
+// Given a String and the length of the actual content, adds a terminating 0,
+// and then adds 0 bytes so that the length becomes a multiple of 4.
+void pad_string_content(String* string, int length) {
+    string->content[length++] = '0';
+    while (length % 4 != 0) {
+        string->content[length++] = '0';
+    }
+}
+
+// Prints a string using IO_out_string.
+void print_string(char* content, int length) {
+    Int l;
+    l.class_tag = 2;
+    l.object_size = 4;
+    l.dispatch_table = 0;
+    l.value = length;
 
     String string;
     string.class_tag = 4;
     string.object_size = 6;
     string.dispatch_table = 0;
-    string.length = &length;
-    assign_string_content(&string, "hello\n\0\0", 8);
+    string.length = &l;
+    assign_string_content(&string, content, length);
+
+    pad_string_content(&string, length);
 
     IO_out_string(&string);
+}
+
+int main() {
+    print_string("hello\n", 6);
+
     return 0;
 }
