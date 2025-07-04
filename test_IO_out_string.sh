e7971447a253e73db6dc8f5a7726749974e2f960
Stan Manilov <stanislav.manilov@gmail.com>
2025-07-04 14:27:07 +0300

    Add expected output for test_IO_out_string

--- test_IO_out_string.sh
+++ test_IO_out_string.sh
@@ -1,4 +1,10 @@
 riscv64-elf-gcc -c -mabi=ilp32 -march=rv32izicsr -nostdlib cc-rv-rt.s
 sed "s/IO\.out_string/IO_out_string/" -i cc-rv-rt.o 
 riscv64-elf-gcc -mabi=ilp32 -march=rv32izicsr -nostdlib -o a.out cc-rv-rt.o test_IO_out_string.c -T cc-rv-rt.ld
-spike --isa=RV32IZICSR a.out
+spike --isa=RV32IZICSR a.out > test_IO_out_string.out
+
+if diff -q test_IO_out_string.out test_IO_out_string.expected > /dev/null; then
+    echo "PASSED"
+else
+    echo "FAILED"
+fi
