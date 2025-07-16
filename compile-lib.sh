030e88b651255f72a0691972f98eef70d4af6e2f
Stan Manilov <stanislav.manilov@gmail.com>
2025-07-16 10:56:09 +0300

    Split compile.sh into compile-lib.sh and self

---
+++ compile-lib.sh
@@ -0,0 +1,1 @@
+riscv64-elf-gcc -c -mabi=ilp32 -march=rv32imzicsr -nostdlib cc-rv-rt.s
--- compile.sh
+++ compile.sh
@@ -1,2 +1,2 @@
-riscv64-elf-gcc -c -mabi=ilp32 -march=rv32imzicsr -nostdlib cc-rv-rt.s
+./compile-lib.sh
 riscv64-elf-gcc -mabi=ilp32 -march=rv32imzicsr -nostdlib cc-rv-rt.o hello.s -T cc-rv-rt.ld -o a.out
