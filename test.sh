709ef2d3e0ace72528f3fb3abb00a047f162cf40
Stan Manilov <stanislav.manilov@gmail.com>
2025-07-19 15:42:25 +0300

    Restore test_Object_abort

--- test.sh
+++ test.sh
@@ -8,13 +8,17 @@ riscv64-elf-gcc -c -mabi=ilp32 -march=rv32imzicsr -nostdlib cc-rv-rt.s
 for test_source in $(ls tests/test_*.s); do
 	test_file=$(basename $test_source)
     testname="${test_file%.s}"
+    input_file="tests/${testname}.in"
+    if [[ ! -f "$input_file" ]]; then
+        input_file="/dev/null"
+    fi
 
     riscv64-elf-gcc -mabi=ilp32 -march=rv32imzicsr -nostdlib cc-rv-rt.o ${test_source} -T cc-rv-rt.ld -o a.out
 
     if $bless; then
-        spike --isa=RV32IMZICSR a.out < tests/${testname}.in > tests/${testname}.expected.out
+        spike --isa=RV32IMZICSR a.out < "$input_file" > tests/${testname}.expected.out
     else
-        timeout 1s spike --isa=RV32IMZICSR a.out < tests/${testname}.in > tests/${testname}.out
+        timeout 1s spike --isa=RV32IMZICSR a.out < "$input_file" > tests/${testname}.out
         exit_code=$?
         if [ $exit_code -eq 124 ]; then
             echo "Test ${testname} TIMED OUT"
